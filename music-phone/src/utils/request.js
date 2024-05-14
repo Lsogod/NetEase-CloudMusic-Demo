eaa9b6d36a33c119ad362b824fd218591e135334
tao <94625948+qte123@users.noreply.github.com>
2024-05-14 16:02:41 +0800

    Update request.js

--- music-phone/src/utils/request.js
+++ music-phone/src/utils/request.js
@@ -1,14 +1,11 @@
 // 网络请求方法
 import axios from "axios";
-
+//导入访问云端的基地址
+const CLOUD_BASE_URL=process.env.VUE_APP_CLOUD_BASE_URL
 const ajax = axios.create({
   //自定义axios对象(ajax就是axios)
-  baseURL: "http://cloud.tafleo.top", //服务基地址
-<<<<<<< HEAD
-  //baseURL: 'http://localhost:3000', //服务基地址
-=======
-  // baseURL: 'http://localhost:3000', //服务基地址
->>>>>>> abc7e501e87cd7847c0a398950eb80ef431fd8e1
+  baseURL: CLOUD_BASE_URL, //服务基地址
+  timeout: 60000, //超时时间  60s 
 });
 
 export default ajax; //相当于导出了一个axios函数
